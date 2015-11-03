5e2126bd83ede331f4feb1f6d74ef10a46c6c845
Takayuki Matsubara <takayuki.1229@gmail.com>
2015-11-03 17:29:24 +0900

    bump version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Luhn.Mixfile do
 
   def project do
     [app: :luhn,
-     version: "0.2.0",
+     version: "0.3.0",
      elixir: "~> 1.0",
      description: "Luhn algorithm in Elixir",
      package: [
