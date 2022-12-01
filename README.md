c4f1b1b75e27ddd25913e9b365df512c304553da
James Every <devstopfix@gmail.com>
2022-12-01 18:19:54 +0000

    Fork repo (#11)

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Luhn algorithm in Elixir
+# Luhn Algorithm
+
+Validate a variety of numbers such as credit cards and national identification numbers with the [Luhn alogrithm][luhn] published in 1960 by [Hans Peter Luhn][hpl].
 
 [![hex.pm version](https://img.shields.io/hexpm/v/luhn.svg)](https://hex.pm/packages/luhn)
 [![hex.pm daily downloads](https://img.shields.io/hexpm/dd/luhn.svg)](https://hex.pm/packages/luhn)
@@ -6,23 +8,28 @@
 [![hex.pm downloads](https://img.shields.io/hexpm/dt/luhn.svg)](https://hex.pm/packages/luhn)
 [![Build Status](https://github.com/ma2gedev/luhn_ex/workflows/Elixir%20CI/badge.svg?branch=master)](https://github.com/ma2gedev/luhn_ex/actions?query=workflow%3A%22Elixir+CI%22)
 
-Validate Luhn number.
+The original [v0.3.3][fork] by [Takayuki Matsubara][ma2gedev] only validated numbers and is available at `{:luhn, "~> 0.3.3"}`.
+
+This fork is available at `{:luhn60, "~> 1.3"}` and has these enhancements:
+
+- &#9745; CI for latest Elixir
+- &#9745; Property tests
+- &#9744; Compute check digit
+- &#9744; Append check digit
+- &#9744; Hexadecimal support
+
+[![hex.pm version](https://img.shields.io/hexpm/v/luhn60.svg)](https://hex.pm/packages/luhn60)
+[![Elixir CI](https://github.com/devstopfix/luhn_ex/actions/workflows/elixir.yml/badge.svg)](https://github.com/devstopfix/luhn_ex/actions/workflows/elixir.yml)
 
 ## Installation
 
-```elixir
-# mix.exs
-defp deps do
-  [
-    {:luhn, "~> 0.3.0"}
-  ]
-end
-```
+This fork:
 
-and fetch
-```bash
-$ mix deps.get
-```
+    {:luhn60, "~> 1.3"}
+
+Original:
+
+    {:luhn, "~> 0.3"}
 
 ## How to use
 
@@ -51,3 +58,8 @@ Takayuki Matsubara (@ma2ge on twitter)
 ## LICENSE
 
 MIT
+
+[fork]: https://github.com/ma2gedev/luhn_ex/tree/0.3.3
+[luhn]: https://en.wikipedia.org/wiki/Luhn_algorithm
+[hpl]: https://en.wikipedia.org/wiki/Hans_Peter_Luhn
+[ma2gedev]: https://github.com/ma2gedev
--- mix.exs
+++ mix.exs
@@ -2,20 +2,24 @@ defmodule Luhn.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :luhn,
-     version: "0.3.3",
-     elixir: "~> 1.10",
-     elixirc_paths: elixirc_paths(Mix.env()),
-     description: "Luhn algorithm in Elixir",
-     package: [
-       maintainers: ["Takayuki Matsubara"],
-       licenses: ["MIT"],
-       links: %{"GitHub" => "https://github.com/ma2gedev/luhn_ex"}
-     ],
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     test_coverage: [tool: ExCoveralls],
-     deps: deps()
+    [
+      app: :luhn60,
+      version: "1.3.3",
+      elixir: "~> 1.10",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      description: "Luhn algorithm in Elixir",
+      package: [
+        maintainers: ["Takayuki Matsubara"],
+        licenses: ["MIT"],
+        links: %{
+          "GitHub" => "https://github.com/ma2gedev/luhn_ex",
+          "Fork" => "https://github.com/devstopfix/luhn_ex"
+        }
+      ],
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      deps: deps()
     ]
   end
 
@@ -23,21 +27,14 @@ defmodule Luhn.Mixfile do
     [applications: [:logger], extra_applications: [:propcheck]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
-    [{:excoveralls, "~> 0.3", only: :dev},
-     {:power_assert, "~> 0.2.1", only: :test},
-     {:ex_doc, "~> 0.22.0", only: :dev, runtime: false},
-     {:benchfella, "~> 0.3", only: :bench},
-     {:propcheck, "~> 1.4", only: [:test, :dev]}]
+    [
+      {:excoveralls, "~> 0.3", only: :dev},
+      {:power_assert, "~> 0.2.1", only: :test},
+      {:ex_doc, "~> 0.22.0", only: :dev, runtime: false},
+      {:benchfella, "~> 0.3", only: :bench},
+      {:propcheck, "~> 1.4", only: [:test, :dev]}
+    ]
   end
 
   defp elixirc_paths(:test), do: ["test/helpers"] ++ elixirc_paths(:prod)
